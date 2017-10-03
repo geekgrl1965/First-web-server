c9f7971198c289bc2f65f7e0bbddc33e81d90ea9
Karen <geekgrl1@hotmail.com>
2017-10-03 15:34:13 -0400

    Add files via upload

--- server.rb
+++ server.rb
@@ -1,9 +1,10 @@
 require 'sinatra'
 
-get '/' do   #when a "get" request comes in, perform this action
+get '/' do
   redirect '/hello.txt'
 end
 
+#when a "get" request comes in, perform this action
 # get "/sinatra" do
 #   "Hello Sinatra"
 # end
